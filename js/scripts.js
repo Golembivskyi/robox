bcb1d4cb18964dbd8b0d778c6c9415a86c970ad3
Belnarodtribunal <73668753+Belnarodtribunal@users.noreply.github.com>
2021-01-12 19:39:39 +0200

    Smooth scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,47 @@
 let burger = document.getElementById('burger'),
-	 nav    = document.getElementById('main-nav'),
-	 slowmo = document.getElementById('slowmo');
+	 nav    = document.getElementById('main-nav');
 
 burger.addEventListener('click', function(e){
 	this.classList.toggle('is-open');
 	nav.classList.toggle('is-open');
 });
 
-slowmo.addEventListener('click', function(e){
-	this.classList.toggle('is-slowmo');
-});
 
 /* Onload demo - dirty timeout */
 let clickEvent = new Event('click');
 
 window.addEventListener('load', function(e) {
-	slowmo.dispatchEvent(clickEvent);
 	burger.dispatchEvent(clickEvent);
 	
 	setTimeout(function(){
 		burger.dispatchEvent(clickEvent);
-		
-		setTimeout(function(){
-			slowmo.dispatchEvent(clickEvent);
-		}, 3500);
 	}, 5500);
 });
 
 
-// Smooth Scroll
-
+// Smooth scroll.
 (() => {
-	let easeInOutCubic = t => t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1;
+	let easeInOutQuad = t => t<.5 ? 2*t*t : -1+(4-2*t)*t;
 	
 	document.addEventListener('click', e => {
 		let a = e.target;
+
+		while (a !== null && a.tagName !== 'A') {
+			a = a.parentNode;
+		}
+
 		let d = document.documentElement;
-		if (a.tagName === 'A' && a.getAttribute('href')[0] === '#') {
+		if (a.getAttribute('href')[0] === '#') {
 			let t = document.querySelector(a.getAttribute('href'));
-			let rect = getBoundingClientRect(t);
-			let dist = rect.top - d.scrollTop;
+			let rect = t.getBoundingClientRect();
+			let dist = rect.top;
 			let startTime = Date.now();
 			let duration = 500;
 			let scrollTop = d.scrollTop;
 			let f = () => {
-				let k = duration / (Date.now() - startTime);
+				let k = (Date.now() - startTime) / duration;
 				if (k > 1) k = 1;
-				d.scrollTop = scrollTop + (dist * k);
+				d.scrollTop = scrollTop + (dist * easeInOutQuad(k));
 				if (k < 1) requestAnimationFrame(f);
 			};
 			requestAnimationFrame(f);
