371b98e6b164a64966c7090ed7dc40fd205510bb
Golembivskyi <volodymyrgolembivskyi@gmail.com>
2021-01-12 19:28:32 +0200

    monor changes

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,30 @@ window.addEventListener('load', function(e) {
 		}, 3500);
 	}, 5500);
 });
+
+
+// Smooth Scroll
+
+(() => {
+	let easeInOutCubic = t => t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1;
+	
+	document.addEventListener('click', e => {
+		let a = e.target;
+		let d = document.documentElement;
+		if (a.tagName === 'A' && a.getAttribute('href')[0] === '#') {
+			let t = document.querySelector(a.getAttribute('href'));
+			let rect = getBoundingClientRect(t);
+			let dist = rect.top - d.scrollTop;
+			let startTime = Date.now();
+			let duration = 500;
+			let scrollTop = d.scrollTop;
+			let f = () => {
+				let k = duration / (Date.now() - startTime);
+				if (k > 1) k = 1;
+				d.scrollTop = scrollTop + (dist * k);
+				if (k < 1) requestAnimationFrame(f);
+			};
+			requestAnimationFrame(f);
+		}
+	});
+})();
